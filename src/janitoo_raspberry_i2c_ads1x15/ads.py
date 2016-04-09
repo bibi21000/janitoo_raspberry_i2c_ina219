deae9e7ef8464324bb8ba3180a16a1118c39594f
bibi21000 <bibi21000@gmail.com>
2016-04-10 00:41:35 +0200

    Updates for docker

--- src/janitoo_raspberry_i2c_ads1x15/ads.py
+++ src/janitoo_raspberry_i2c_ads1x15/ads.py
@@ -108,9 +108,7 @@ class ADSComponent(JNTComponent):
         """Check that the component is 'available'
 
         """
-        if 'temperature' not in self.values:
-            return False
-        return self.values['temperature'].data is not None
+        return self.sensor is not None
 
     def start(self, mqttc):
         """Start the bus
