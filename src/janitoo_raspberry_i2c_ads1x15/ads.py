53750ed2d8137e95f78fcec0a8b48ab7f614a756
bibi21000 <bibi21000@gmail.com>
2016-05-02 18:07:48 +0200

    Updates for tests

--- src/janitoo_raspberry_i2c_ads1x15/ads.py
+++ src/janitoo_raspberry_i2c_ads1x15/ads.py
@@ -68,9 +68,8 @@ class ADSComponent(JNTComponent):
         name = kwargs.pop('name', "Input")
         product_name = kwargs.pop('product_name', "ADS")
         product_type = kwargs.pop('product_type', "Analog to binary converter")
-        product_manufacturer = kwargs.pop('product_manufacturer', "Janitoo")
         JNTComponent.__init__(self, oid=oid, bus=bus, addr=addr, name=name,
-                product_name=product_name, product_type=product_type, product_manufacturer="Janitoo", **kwargs)
+                product_name=product_name, product_type=product_type, **kwargs)
         logger.debug("[%s] - __init__ node uuid:%s", self.__class__.__name__, self.uuid)
 
         uuid="addr"
